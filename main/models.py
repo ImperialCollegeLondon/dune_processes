9cf74c73fbd612d3cf12b025a8cfb71e72f83ee1
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-08-14 12:47:01 +0100

    Create custom user model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+"""Models module for the main app."""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models  # noqa: F401
+
+
+class User(AbstractUser):
+    """Custom user model for this project."""
