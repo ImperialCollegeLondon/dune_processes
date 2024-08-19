5362e3cf4d85481a30eb24e9508815caed5aee9c
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-08-19 10:52:48 +0100

    Remove django db mark from test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,6 @@
-import pytest
 from pytest_django.asserts import assertTemplateUsed
 
 
-@pytest.mark.django_db
 def test_index(client, admin_client):
     """Test the index view."""
     with assertTemplateUsed(template_name="main/index.html"):
