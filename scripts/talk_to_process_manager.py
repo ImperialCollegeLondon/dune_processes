c7ab9bec6ad59944f28de5b813f30dfb9a5ca0df
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2024-08-22 15:57:03 +0100

    Add demo script for talking to the process manager

---
+++ scripts/talk_to_process_manager.py
@@ -0,0 +1,53 @@
+"""Example script used to check communication with the process manager.
+
+This is intended to be run within docker from the `app` service, i.e.:
+
+```
+docker compose exec app python scripts/talk_to_process_manager.py
+```
+
+and provides a basic proof of principle of communicating with the process manager via
+gRPC.
+
+The script starts a dummy session on the process manager and then retrieves the details
+of all sessions and prints them to stdout.
+"""
+
+import asyncio
+
+from drunc.process_manager.process_manager_driver import ProcessManagerDriver
+from drunc.utils.shell_utils import create_dummy_token_from_uname
+from druncschema.process_manager_pb2 import ProcessQuery
+
+
+async def create_session(pmd: ProcessManagerDriver):
+    """Create a process manager session using the dummy_boot command."""
+    return [
+        item
+        async for item in pmd.dummy_boot(
+            user="DUMMY_USER",
+            session_name="sess_name",
+            n_processes=1,
+            sleep=2,
+            n_sleeps=2,
+        )
+    ]
+
+
+async def get_session_info(pmd: ProcessManagerDriver):
+    """Get info about all sessions from process manager."""
+    query = ProcessQuery(names=[".*"])
+    return await pmd.ps(query)
+
+
+async def main():
+    """Run the script."""
+    token = create_dummy_token_from_uname()
+    pmd = ProcessManagerDriver("drunc:10054", token=token, aio_channel=True)
+
+    await create_session(pmd)
+    return await get_session_info(pmd)
+
+
+val = asyncio.run(main())
+print(val.data)
