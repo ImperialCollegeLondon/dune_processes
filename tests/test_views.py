f6defa43510cc7e7b897a80bf32eaa2c796a65d8
Ryan Smith <ryan.smith@imperial.ac.uk>
2024-08-28 17:19:36 +0100

    Mocking get_session_info function

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,9 @@
 from pytest_django.asserts import assertTemplateUsed
 
 
-def test_index(client, admin_client):
+def test_index(client, admin_client, mocker):
     """Test the index view."""
+    mocker.patch("main.views.get_session_info")
     with assertTemplateUsed(template_name="main/index.html"):
         response = client.get("/")
     assert response.status_code == 200
