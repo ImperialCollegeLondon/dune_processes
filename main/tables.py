0469694f00f98056f2bee875b7fc067f247db7e6
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-08-30 09:58:16 +0100

    Uncomment section that shouldn't be commented

--- main/tables.py
+++ main/tables.py
@@ -6,9 +6,9 @@ import django_tables2 as tables
 class ProcessTable(tables.Table):
     """Defines and Process Table for the data from the Process Manager."""
 
-    uuid = tables.Column()  # verbose_name="UUID")
+    uuid = tables.Column(verbose_name="UUID")
     name = tables.Column(verbose_name="Name")
     user = tables.Column(verbose_name="User")
     session = tables.Column(verbose_name="Session")
     status_code = tables.Column(verbose_name="Status Code")
-    exit_code = tables.Column()  # verbose_name="Exit Code")
+    exit_code = tables.Column(verbose_name="Exit Code")
