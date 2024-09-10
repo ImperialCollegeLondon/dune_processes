910e18ae290a5d7baedb06676936e19b2eb12d31
James Paul Turner <james.turner@imperial.ac.uk>
2024-09-10 11:03:34 +0100

    Add view fn to kill process.

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@
 
 import asyncio
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from drunc.process_manager.process_manager_driver import ProcessManagerDriver
 from drunc.utils.shell_utils import create_dummy_token_from_uname
@@ -10,15 +10,21 @@ from druncschema.process_manager_pb2 import (
     ProcessInstance,
     ProcessInstanceList,
     ProcessQuery,
+    ProcessUUID,
 )
 
 from .tables import ProcessTable
 
 
+def get_process_manager_driver() -> ProcessManagerDriver:
+    """Get a ProcessManagerDriver instance."""
+    token = create_dummy_token_from_uname()
+    return ProcessManagerDriver("drunc:10054", token=token, aio_channel=True)
+
+
 async def get_session_info() -> ProcessInstanceList:
     """Get info about all sessions from process manager."""
-    token = create_dummy_token_from_uname()
-    pmd = ProcessManagerDriver("drunc:10054", token=token, aio_channel=True)
+    pmd = get_process_manager_driver()
     query = ProcessQuery(names=[".*"])
     return await pmd.ps(query)
 
@@ -47,3 +53,28 @@ def index(request: HttpRequest) -> HttpResponse:
     context = {"table": ProcessTable(table)}
 
     return render(request=request, context=context, template_name="main/index.html")
+
+
+async def _kill_process_call(uuid: str) -> None:
+    """Kill a process with a given UUID.
+
+    Args:
+        uuid: UUID of the process to kill.
+    """
+    pmd = get_process_manager_driver()
+    query = ProcessQuery(uuids=[ProcessUUID(uuid=uuid)])
+    await pmd.kill(query)
+
+
+def kill_process(request: HttpRequest, uuid: str) -> HttpResponse:
+    """Kill a process with a given UUID.
+
+    Args:
+        request: Django HttpRequest object (unused, but required by Django).
+        uuid: UUID of the process to kill.
+
+    Returns:
+        HttpResponse redirecting to the index page.
+    """
+    asyncio.run(_kill_process_call(uuid))
+    return HttpResponseRedirect("main:index")
