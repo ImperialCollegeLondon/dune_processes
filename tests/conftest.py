679f2174865a2823393a5b9159d6d56bc309ba84
James Paul Turner <jamesturner246@users.noreply.github.com>
2024-09-11 20:51:04 +0100

    Simplify create user fixture.
    Co-authored-by: Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,7 @@ from django.test import Client
 @pytest.fixture()
 def auth_client(django_user_model):
     """Return an authenticated client."""
-    username = "testuser"
-    email = "testuser@domain"
-    password = "password"
-    user = django_user_model.objects.create_user(
-        username=username, email=email, password=password
-    )
+    user = django_user_model.objects.create(username="testuser")
     client = Client()
     client.force_login(user)
     return client
