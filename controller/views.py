e52c44ffef496bd39392f2a8a75b5ecea09cf7e0
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-10-03 14:25:37 +0100

    Create index view for controller

--- controller/views.py
+++ controller/views.py
@@ -1,3 +1,11 @@
 """Views module for the controller app."""
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    """View that renders the index/home page."""
+    return render(request=request, template_name="controller/index.html")
