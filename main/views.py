901be01ac35fa0e7108972f4f1eba661629d129b
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-10-04 15:35:13 +0100

    Require login for main index

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 """Views for the main app."""
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View that renders the index/home page."""
     return render(request=request, template_name="main/index.html")
