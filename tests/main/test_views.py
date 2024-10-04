14d7dedb02daf7b05ef409a2869fb041c1a31db2
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-10-04 16:52:11 +0100

    Assert the main index view contains buttons and nav links

--- tests/main/test_views.py
+++ tests/main/test_views.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.urls import reverse
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertContains, assertNotContains, assertTemplateUsed
 
 from ..utils import LoginRequiredTest
 
@@ -11,8 +11,30 @@ class TestIndexView(LoginRequiredTest):
 
     endpoint = reverse("main:index")
 
-    def test_index_view_authenticated(self, auth_client, mocker):
+    def test_no_nav_links(self, client):
+        """Test that the navbar does not have any nav links when not authenticated."""
+        response = client.get(self.endpoint, follow=True)
+        assertNotContains(response, "nav-link")
+
+    def test_index_view_authenticated(self, auth_client):
         """Test the index view for an authenticated user."""
         with assertTemplateUsed(template_name="main/index.html"):
             response = auth_client.get(self.endpoint)
         assert response.status_code == HTTPStatus.OK
+
+        assertContains(
+            response,
+            f'<a class="nav-link" href="{reverse("process_manager:index")}">Process Manager</a>',  # noqa: E501
+        )
+        assertContains(
+            response,
+            f'<a href="{reverse("process_manager:index")}" class="btn btn-secondary">Process Manager</a>',  # noqa: E501
+        )
+        assertContains(
+            response,
+            f'<a class="nav-link" href="{reverse("controller:index")}">Controller</a>',
+        )
+        assertContains(
+            response,
+            f'<a href="{reverse("controller:index")}" class="btn btn-secondary">Controller</a>',  # noqa: E501
+        )
