9ee5c6329064502be12e802261ed985eb13be5ee
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-10-04 16:11:42 +0100

    Remove process_manager views tests from main views tests

---
+++ tests/main/test_views.py
@@ -0,0 +1,19 @@
+from http import HTTPStatus
+
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+from .utils import LoginRequiredTest
+
+
+class TestIndexView(LoginRequiredTest):
+    """Tests for the index view."""
+
+    endpoint = reverse("main:index")
+
+    def test_index_view_authenticated(self, auth_client, mocker):
+        """Test the index view for an authenticated user."""
+        mocker.patch("main.views.get_session_info")
+        with assertTemplateUsed(template_name="main/index.html"):
+            response = auth_client.get(self.endpoint)
+        assert response.status_code == HTTPStatus.OK
